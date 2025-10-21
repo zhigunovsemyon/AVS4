50bfbd0551369e08608c462211c9b8b3de189ac4
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-21 15:42:46 +0300

    Сделал левые прямоугольники

--- src/main.c
+++ src/main.c
@@ -3,23 +3,31 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define BEGIN 1.0
-#define END 3.0
-
-double func(double x)
+double fifth(double x)
 {
 	return asin(1.0 / sqrt(1.1 + x));
 }
 
+double left_rect(double begin, double end, int num_steps, double(*func)(double))
+{
+	double sum = 0.0;
+	double step = (end - begin)/ (double)num_steps;
+
+	#pragma omp parallel for reduction(+:sum)
+	for(int i = 0; i <= num_steps; ++i){
+		double x = begin + i * step;
+		double h = func(x);
+		sum += h * step;
+	}
+
+	return sum;
+}
+
+#define BEGIN 1.0
+#define END 3.0
+
 int main(void)
 {
-	int num_steps = 10;
-	double step = (END - BEGIN)/ (double)num_steps;
-
-	// #pragma omp parallel for
-	for(int i = 0; i <= num_steps; ++i){
-		double x = BEGIN + i * step;
-		printf("f(%lf) = %lf\n", x, func(x));
-	}
+	printf("%lf\n", left_rect(BEGIN, END, 1488, fifth));
 	return 0;
 }
