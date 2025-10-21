5284a74cd428376d05de9dea3d8255af75bd9fbb
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-10-21 16:34:05 +0300

    Переделал под колбеки calculate()

--- src/main.c
+++ src/main.c
@@ -25,18 +25,25 @@ left_rect_calc(double begin, double end, int num_steps, double (*func)(double))
 	return sum;
 }
 
-void left_rect(double epsilon, double begin, double end, double (*func)(double))
+void calculate(double epsilon,
+	       double begin,
+	       double end,
+	       double (*method)(double begin,
+				double end,
+				int num_steps,
+				double (*)(double)),
+	       double (*math_func)(double))
 {
 	double prev = INFINITY;
 
 	double start_time = omp_get_wtime();
 	for (int num_steps = 1;; num_steps++) {
-		double res = left_rect_calc(begin, end, num_steps, func);
+		double res = method(begin, end, num_steps, math_func);
 
 		if (fabs(res - prev) < epsilon) {
 			double dt = omp_get_wtime() - start_time;
-			printf("Методом левых прямоугольников точность %.1le "
-			       "достигнута за %lf с. Число шагов: %d\n",
+			printf("Точность %.1le достигнута за %lf с. Число "
+			       "шагов: %d\n",
 			       epsilon, dt, num_steps);
 			printf("Результат: %.10lf\n", res);
 			return;
@@ -49,8 +56,9 @@ int main(void)
 {
 	double const BEGIN = 1.0, END = 3.0;
 
-	left_rect(0.00001, BEGIN, END, fifth);
-	left_rect(0.000001, BEGIN, END, fifth);
-	left_rect(0.0000001, BEGIN, END, fifth);
+	puts("Метод левых прямоугольников:");
+	calculate(0.00001, BEGIN, END, left_rect_calc, fifth);
+	calculate(0.000001, BEGIN, END, left_rect_calc, fifth);
+	calculate(0.0000001, BEGIN, END, left_rect_calc, fifth);
 	return 0;
 }
