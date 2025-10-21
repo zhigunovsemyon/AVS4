6110c821a8290cc829328e6221e2817aa252291b
Semyon <zhigunovsemen@gmail.com>
2025-10-21 19:41:59 +0300

    Добавил метод трапеций

--- src/main.c
+++ src/main.c
@@ -2,11 +2,28 @@
 #include <omp.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 double fifth(double x)
 {
 	return asin(1.0 / sqrt(1.1 + x));
+}
+
+double
+trapezoid_calc(double begin, double end, int num_steps, double (*func)(double))
+{
+	double sum = 0.0;
+	double step = (end - begin) / (double)num_steps;
+
+#pragma omp parallel for reduction(+ : sum)
+	for (int i = 1; i <= num_steps; ++i) {
+		double x1 = begin + i * step;
+		double x2 = x1 - step;
+
+		double h = 0.5 * (func(x2) + func(x1));
+		sum += h * step;
+	}
+
+	return sum;
 }
 
 double
@@ -105,5 +122,13 @@ int main(void)
 	calculate(0.0000001, BEGIN, END, centre_rect_calc, fifth);
 	calculate(0.000000000000001, BEGIN, END, centre_rect_calc, fifth);
 	putchar('\n');
+
+	puts("Метод трапеций:");
+	calculate(0.00001, BEGIN, END, trapezoid_calc, fifth);
+	calculate(0.000001, BEGIN, END, trapezoid_calc, fifth);
+	calculate(0.0000001, BEGIN, END, trapezoid_calc, fifth);
+	calculate(0.000000000000001, BEGIN, END, trapezoid_calc, fifth);
+	putchar('\n');
+
 	return 0;
 }
